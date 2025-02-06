cea0d5447d927d67c5ab57941be5f24b81a09dd5
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-02-06 22:46:14 +0545

    Frontend changes

--- frontend/src/pages/homes/Banner.jsx
+++ frontend/src/pages/homes/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
             <h1 className='md:text-5xl text-2xl font-medium mb-7'>New Releases This Week</h1>
             <p className='mb-10'>It's time to update your reading list with some of the latest and greatest releases in the literary world. From heart-pumping thrillers to captivating memoirs, this week's new releases offer something for everyone</p>
 
-            <button className='btn-primary'>Subscribe</button>
+            <button className='bg-green-500 hover:bg-green-700 text-white rounded-lg shadow-md px-4 py-2'>Subscribe</button>
         </div>
 
        
